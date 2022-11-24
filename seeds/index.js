b3c01d7d1aecb416379d9fa57fe6943e80f956c5
Gator <allidodt@gmail.com>
2022-11-23 16:58:58 -0800

    seeds index

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const sequelize = require("../config/connection");
+const seedUser = require("./userData");
+const seedComments = require("./commentData");
+const seedPost = require("./postData");
+
+const seedUser = require("./userData");
+const seedComments = require("./commentData");
+const seedPost = require("./postData");
+
+const seedAll = async () => {
+    await sequelize.sync({force: true});
+    console.log("DATABASE SYNCED");
+
+    await seedUser();
+    console.log("DATABASE SYNCED");
+
+    await seedComments();
+    console.log("DATABASE SYNCED");
+
+    await seedPost();
+    console.log("DATABASE SYNCED");
+
+    process.exit(0);
+};
+
+seedAll();
