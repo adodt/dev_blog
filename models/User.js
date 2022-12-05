bd3472de6fea2a6b129ec1037f6027232843bf26
Gator <allidodt@gmail.com>
2022-12-05 15:18:23 -0800

    updated models and handlebars to display

--- models/User.js
+++ models/User.js
@@ -20,24 +20,9 @@ User.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        name: {
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isEmail: true,
-            },
         },
         password: {
             type: DataTypes.STRING,
